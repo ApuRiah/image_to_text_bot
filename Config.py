2a584cf4a0d452c70a1e3b2a8978d0be582b10b9
Hashem Alsudmi <alsdmymhmd682@gmail.com>
2024-01-06 02:08:25 +0300

    Initial commit

--- Config.py
+++ Config.py
@@ -1,5 +1,5 @@
 """the ApI Key from https://ocr.space/ocrapi after logging in and choosing the type of API ,they will send a key to your email """
-API_KEY = "K81610801188957"
+API_KEY = "your API key "
 # api key from https://ocr.space/ocrapi
-BOT_TOKEN = "6515139562:AAFjM1fQ-uuMVFLrwEVA7k9SV6liRujIvHo"
+BOT_TOKEN = "your bot token"
 # bot token from @BotFather
